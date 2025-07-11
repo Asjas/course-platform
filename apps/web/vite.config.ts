c4da12628f1575a6735c0cf58ccaa2308c1067d4
A-J Roos <asjas@outlook.com>
2025-07-11 10:32:28 +0200

    add pwa

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,8 +4,8 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tanstackRouter({
@@ -16,6 +16,19 @@ export default defineConfig({
     react(),
     tailwindcss(),
     cloudflare(),
+    VitePWA({
+      injectRegister: "inline",
+      registerType: "autoUpdate",
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallback: "index.html",
+        type: "module",
+      },
+    }),
   ],
   resolve: {
     alias: {
