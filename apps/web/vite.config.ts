9a205310bde30757f8160f47334b8601dfeb65b1
A-J Roos <asjas@outlook.com>
2025-07-11 10:33:35 +0200

    add pwa

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -8,6 +8,42 @@ import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
+    VitePWA({
+      injectRegister: "inline",
+      registerType: "autoUpdate",
+      manifestFilename: "manifest.json",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}", "manifest.json"],
+        clientsClaim: true,
+        skipWaiting: true,
+        cleanupOutdatedCaches: true,
+      },
+      manifest: {
+        short_name: "Codewizard Training",
+        name: "Codewizard Training",
+        icons: [
+          {
+            src: "favicon.ico",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon",
+          },
+          {
+            src: "logo192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "logo512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+        ],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+      },
+    }),
     tanstackRouter({
       target: "react",
       autoCodeSplitting: true,
@@ -16,19 +52,6 @@ export default defineConfig({
     react(),
     tailwindcss(),
     cloudflare(),
-    VitePWA({
-      injectRegister: "inline",
-      registerType: "autoUpdate",
-      workbox: {
-        clientsClaim: true,
-        skipWaiting: true,
-      },
-      devOptions: {
-        enabled: true,
-        navigateFallback: "index.html",
-        type: "module",
-      },
-    }),
   ],
   resolve: {
     alias: {
