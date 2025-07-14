7ed4e925b703d6e08497899fbb5a1b0bd9ef38d4
A-J Roos <asjas@outlook.com>
2025-07-14 09:25:32 +0200

    remove pwa while testing

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,49 +1,49 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
+
+// import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
-    VitePWA({
-      injectRegister: "inline",
-      registerType: "autoUpdate",
-      manifestFilename: "manifest.json",
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg}", "manifest.json"],
-        clientsClaim: true,
-        skipWaiting: true,
-        cleanupOutdatedCaches: true,
-      },
-      manifest: {
-        short_name: "Codewizard Training",
-        name: "Codewizard Training",
-        icons: [
-          {
-            src: "favicon.ico",
-            sizes: "64x64 32x32 24x24 16x16",
-            type: "image/x-icon",
-          },
-          {
-            src: "logo192.png",
-            type: "image/png",
-            sizes: "192x192",
-          },
-          {
-            src: "logo512.png",
-            type: "image/png",
-            sizes: "512x512",
-          },
-        ],
-        start_url: ".",
-        display: "standalone",
-        theme_color: "#000000",
-        background_color: "#ffffff",
-      },
-    }),
+    // VitePWA({
+    //   injectRegister: "inline",
+    //   registerType: "autoUpdate",
+    //   manifestFilename: "manifest.json",
+    //   workbox: {
+    //     globPatterns: ["**/*.{js,css,html,ico,png,svg}", "manifest.json"],
+    //     clientsClaim: true,
+    //     skipWaiting: true,
+    //     cleanupOutdatedCaches: true,
+    //   },
+    //   manifest: {
+    //     short_name: "Codewizard Training",
+    //     name: "Codewizard Training",
+    //     icons: [
+    //       {
+    //         src: "favicon.ico",
+    //         sizes: "64x64 32x32 24x24 16x16",
+    //         type: "image/x-icon",
+    //       },
+    //       {
+    //         src: "logo192.png",
+    //         type: "image/png",
+    //         sizes: "192x192",
+    //       },
+    //       {
+    //         src: "logo512.png",
+    //         type: "image/png",
+    //         sizes: "512x512",
+    //       },
+    //     ],
+    //     start_url: ".",
+    //     display: "standalone",
+    //     theme_color: "#000000",
+    //     background_color: "#ffffff",
+    //   },
+    // }),
     tanstackRouter({
       target: "react",
       autoCodeSplitting: true,
@@ -51,7 +51,6 @@ export default defineConfig({
     }),
     react(),
     tailwindcss(),
-    cloudflare(),
   ],
   resolve: {
     alias: {
