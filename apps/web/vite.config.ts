25a9ea14ac20125f35b9cb2503533a2a0ed37771
A-J Roos <asjas@outlook.com>
2025-07-14 17:48:32 +0200

    update favicon sizes in manifest

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
         icons: [
           {
             src: "favicon.ico",
-            sizes: "64x64 32x32 24x24 16x16",
+            sizes: "64x64",
             type: "image/x-icon",
           },
           {
