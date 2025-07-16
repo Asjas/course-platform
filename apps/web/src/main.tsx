6fa0d41f274d811d8b421b321aa230596f08c1b3
A-J Roos <asjas@outlook.com>
2025-07-16 11:41:10 +0200

    do some work

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,13 +1,10 @@
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import reportWebVitals from "~/reportWebVitals";
+import { routeTree } from "~/routeTree.gen";
+import "~/tailwind.css";
 
-import reportWebVitals from "./reportWebVitals.ts";
-// Import the generated route tree
-import { routeTree } from "./routeTree.gen";
-import "./styles.css";
-
-// Create a new router instance
 const router = createRouter({
   routeTree,
   context: {},
@@ -17,15 +14,14 @@ const router = createRouter({
   defaultPreloadStaleTime: 0,
 });
 
-// Register the router instance for type safety
 declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
   }
 }
 
-// Render the app
 const rootElement = document.getElementById("app");
+
 if (rootElement && !rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
@@ -35,7 +31,4 @@ if (rootElement && !rootElement.innerHTML) {
   );
 }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
