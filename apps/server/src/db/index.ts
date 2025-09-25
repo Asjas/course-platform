e8d9e4c2f4dee23190452522f356ef59041d2198
A-J Roos <asjasroos@pm.me>
2025-09-25 15:46:33 +0200

    feat: update database schema to include multiple models for drizzle ORM
    Signed-off-by: A-J Roos <asjasroos@pm.me>

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,6 +1,13 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Client } from "pg";
 import config from "~/config.js";
+import * as couponSchema from "~/db/schema/coupon.js";
+import * as courseSchema from "~/db/schema/course.js";
+import * as enrollmentSchema from "~/db/schema/enrollment.js";
+import * as progressSchema from "~/db/schema/progress.js";
+import * as purchaseSchema from "~/db/schema/purchase.js";
+import * as supportSchema from "~/db/schema/support.js";
+import * as teamLicenseSchema from "~/db/schema/teamLicense.js";
 import * as userSchema from "~/db/schema/user.js";
 import { DrizzleLogger } from "~/lib/logging.js";
 
@@ -9,7 +16,16 @@ const client = new Client({ connectionString: config.DATABASE_URL });
 await client.connect();
 
 export const db = drizzle({
-  schema: { ...userSchema },
+  schema: {
+    ...couponSchema,
+    ...courseSchema,
+    ...enrollmentSchema,
+    ...progressSchema,
+    ...purchaseSchema,
+    ...supportSchema,
+    ...teamLicenseSchema,
+    ...userSchema,
+  },
   client: client,
   logger: new DrizzleLogger(),
   casing: "snake_case",
