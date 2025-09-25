ebbaba358811cc1c70e59ad1d359fc4acaa98a39
A-J Roos <asjasroos@pm.me>
2025-09-25 16:44:39 +0200

    Add new journal entry and update timestamp defaults in schema helpers
    - Added a new journal entry with index 1 and tag "0001_motionless_mindworm" to _journal.json.
    - Updated the timestamps in columns.helpers.ts to include default values for createdAt and updatedAt fields.
    
    Signed-off-by: A-J Roos <asjasroos@pm.me>

--- apps/server/src/db/schema/columns.helpers.ts
+++ apps/server/src/db/schema/columns.helpers.ts
@@ -2,9 +2,11 @@ import { timestamp } from "drizzle-orm/pg-core";
 
 export const timestamps = {
   createdAt: timestamp({ withTimezone: true })
+    .defaultNow()
     .$defaultFn(() => new Date())
     .notNull(),
   updatedAt: timestamp({ withTimezone: true })
+    .defaultNow()
     .$onUpdateFn(() => new Date())
     .notNull(),
 };
