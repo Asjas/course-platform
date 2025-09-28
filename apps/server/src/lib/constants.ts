376f2a919f0bd1bbcafdafc78651fc133b0a7b4d
A-J Roos <asjasroos@pm.me>
2025-09-28 12:18:12 +0200

    feat: add cache-related constants for public and private revalidation
    Signed-off-by: A-J Roos <asjasroos@pm.me>

--- apps/server/src/lib/constants.ts
+++ apps/server/src/lib/constants.ts
@@ -4,6 +4,33 @@ export const TEN_MINUTES = 600;
 export const THIRTY_MINUTES = 1800;
 export const ONE_HOUR = 3600;
 export const ONE_DAY = 86_400;
-export const ONE_WEEK = 604_800;
-export const ONE_MONTH = 2_629_746;
-export const ONE_YEAR = 31_556_952;
+
+export const CACHE_NO_CACHE = "no-cache";
+export const CACHE_PRIVATE_NO_CACHE = "private, no-cache";
+export const CACHE_NO_STORE = "no-store, max-age=0, must-revalidate";
+
+export const CACHE_PUBLIC_1_MINUTE_REVALIDATE =
+  "public, max-age=60, stale-if-error=3600, must-revalidate";
+export const CACHE_PUBLIC_5_MINUTES_REVALIDATE =
+  "public, max-age=300, stale-if-error=3600, must-revalidate";
+export const CACHE_PUBLIC_10_MINUTES_REVALIDATE =
+  "public, max-age=600, stale-if-error=3600, must-revalidate";
+export const CACHE_PUBLIC_30_MINUTES_REVALIDATE =
+  "public, max-age=1800, stale-if-error=3600, must-revalidate";
+export const CACHE_PUBLIC_1_HOUR_REVALIDATE =
+  "public, max-age=3600, stale-if-error=3600, must-revalidate";
+export const CACHE_PUBLIC_1_DAY_REVALIDATE =
+  "public, max-age=86_400, stale-if-error=3600, must-revalidate";
+
+export const CACHE_PRIVATE_1_MINUTE_REVALIDATE =
+  "private, max-age=60, stale-if-error=3600, must-revalidate";
+export const CACHE_PRIVATE_5_MINUTES_REVALIDATE =
+  "private, max-age=300, stale-if-error=3600, must-revalidate";
+export const CACHE_PRIVATE_10_MINUTES_REVALIDATE =
+  "private, max-age=600, stale-if-error=3600, must-revalidate";
+export const CACHE_PRIVATE_30_MINUTES_REVALIDATE =
+  "private, max-age=1800, stale-if-error=3600, must-revalidate";
+export const CACHE_PRIVATE_1_HOUR_REVALIDATE =
+  "private, max-age=3600, stale-if-error=3600, must-revalidate";
+export const CACHE_PRIVATE_1_DAY_REVALIDATE =
+  "private, max-age=86_400, stale-if-error=3600, must-revalidate";
