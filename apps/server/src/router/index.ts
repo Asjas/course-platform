6817aa1e225bb42093dfe7f897a5e7e469932ee6
A-J Roos <asjasroos@pm.me>
2025-09-30 16:10:39 +0200

    feat: implement initial TRPC router with hello procedure
    Signed-off-by: A-J Roos <asjasroos@pm.me>

---
+++ apps/server/src/router/index.ts
@@ -0,0 +1,10 @@
+import { initTRPC } from "@trpc/server";
+import { z } from "zod";
+
+const t = initTRPC.create();
+export const appRouter = t.router({
+  hello: t.procedure
+    .input(z.object({ name: z.string() }))
+    .query(({ input }) => `Hello, ${input.name}!`),
+});
+export type AppRouter = typeof appRouter;
