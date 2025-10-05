e445f0a6c8db9dd50584151e2e5287532b78577e
A-J Roos <asjasroos@pm.me>
2025-10-05 16:50:23 +0200

    fix: add retries configuration for Cypress end-to-end tests
    Signed-off-by: A-J Roos <asjasroos@pm.me>

--- apps/web/cypress.config.ts
+++ apps/web/cypress.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:4173",
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
   },
 });
