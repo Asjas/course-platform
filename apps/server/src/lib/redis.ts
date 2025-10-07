5e337d7efce2fa4377b14a50496a4b2f11a17df0
A-J Roos <asjasroos@pm.me>
2025-10-07 21:12:39 +0200

    fix: ensure enableAutoPipelining is set in Redis options
    Signed-off-by: A-J Roos <asjasroos@pm.me>

--- apps/server/src/lib/redis.ts
+++ apps/server/src/lib/redis.ts
@@ -5,6 +5,7 @@ const redisOptions: RedisOptions = {
   host: config.REDIS_HOST,
   port: config.REDIS_PORT,
   password: config.REDIS_PASSWORD,
+  enableAutoPipelining: true,
 };
 
 export const redis = new Redis(redisOptions);
