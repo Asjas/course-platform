745f8def0e79dc2aaec8a95717cc134a74325936
A-J Roos <asjasroos@pm.me>
2025-10-14 13:55:49 +0200

    fix(server): register uncaughtException handler to log error and trigger graceful shutdown
    Signed-off-by: A-J Roos <asjasroos@pm.me>

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -5,6 +5,20 @@ import createServer from "~/server.js";
 const PORT = Number(config.PORT) || 3000;
 const app = await createServer(config);
 
+process.on("uncaughtException", (err) => {
+  app.log.error({ err }, "Uncaught Exception occurred");
+
+  closeWithGrace(async function ({ signal, err }) {
+    if (err) {
+      app.log.error({ err }, "Server closing with error");
+    } else {
+      app.log.info(`${signal} received, server closing`);
+    }
+
+    await app.close();
+  });
+});
+
 closeWithGrace(async function ({ signal, err }) {
   if (err) {
     app.log.error({ err }, "Server closing with error");
