cf22ade76d9b84006022fdc69fd7481a5176b625
A-J Roos <asjasroos@pm.me>
2025-10-23 14:29:48 +0200

    chore(drizzle): add statement-breakpoint markers to 0005 migration
    Signed-off-by: A-J Roos <asjasroos@pm.me>

--- apps/server/drizzle/0005_third_roland_deschain.sql
+++ apps/server/drizzle/0005_third_roland_deschain.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "my_schema"."user" DROP COLUMN IF EXISTS "image";
-ALTER TABLE "my_schema"."user" ADD COLUMN "image" bytea;
+ALTER TABLE "my_schema"."user" DROP COLUMN IF EXISTS "image";--> statement-breakpoint
+ALTER TABLE "my_schema"."user" ADD COLUMN "image" bytea;--> statement-breakpoint
