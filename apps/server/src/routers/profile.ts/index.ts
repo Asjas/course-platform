c8d4b5291e965b93c933d490dfee4b8d4ca34ac0
A-J Roos <asjasroos@pm.me>
2025-10-28 08:59:23 +0200

    refactor(profile,trpc): use isAuthenticated middleware for getPresignedUrl and remove manual auth check/TRPCError
    Signed-off-by: A-J Roos <asjasroos@pm.me>

--- apps/server/src/routers/profile.ts/index.ts
+++ apps/server/src/routers/profile.ts/index.ts
@@ -1,24 +1,17 @@
-import { TRPCError } from "@trpc/server";
 import * as z from "zod";
 import { generatePresignedUploadUrl } from "~/lib/r2-upload.js";
-import { publicProcedure, router } from "~/router.js";
+import { isAuthenticated, publicProcedure, router } from "~/router.js";
 
 export const profileRouter = router({
   getPresignedUrl: publicProcedure
+    .use(isAuthenticated)
     .input(
       z.object({
         filename: z.string().min(1),
         contentType: z.string(),
       }),
     )
-    .mutation(async ({ ctx, input }) => {
-      if (!ctx.user) {
-        throw new TRPCError({
-          code: "UNAUTHORIZED",
-          message: "You are not authorized to access this resource",
-        });
-      }
-
+    .mutation(async ({ input }) => {
       const key = `profile_images/${input.filename}`;
       const { presignedUrl, publicUrl } = await generatePresignedUploadUrl({
         key,
