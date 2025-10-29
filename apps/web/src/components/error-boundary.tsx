ea7c99cf186296ab4e7111ce81cb4051b35595f2
A-J Roos <asjasroos@pm.me>
2025-10-29 13:24:58 +0200

    feat(web): add ErrorBoundary component to reset React Query error boundary and provide reload button
    Signed-off-by: A-J Roos <asjasroos@pm.me>

---
+++ apps/web/src/components/error-boundary.tsx
@@ -0,0 +1,28 @@
+import { useQueryErrorResetBoundary } from "@tanstack/react-query";
+import { useRouter } from "@tanstack/react-router";
+import { useEffect } from "react";
+
+export default function ErrorBoundaryComponent({ error }: { error: Error }) {
+  const router = useRouter();
+  const queryErrorResetBoundary = useQueryErrorResetBoundary();
+
+  useEffect(() => {
+    queryErrorResetBoundary.reset();
+  }, [queryErrorResetBoundary]);
+
+  return (
+    <div className="flex h-full flex-col items-center justify-center gap-4 text-center text-red-600">
+      {error.message}
+      <button
+        className="rounded-md bg-red-600 px-4 py-2 text-white hover:bg-red-800"
+        type="reset"
+        onClick={(event) => {
+          event.preventDefault();
+          router.invalidate();
+        }}
+      >
+        Reload page
+      </button>
+    </div>
+  );
+}
