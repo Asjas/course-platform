1491c1aebf1be8d80f8a828a509a313055ff2bba
A-J Roos <asjasroos@pm.me>
2025-11-18 08:19:06 +0200

    feat(utils): add listWithAnd function for formatted string output
    Signed-off-by: A-J Roos <asjasroos@pm.me>

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -8,3 +8,10 @@ export function cn(...inputs: ClassValue[]) {
 export function capitalizeFirstLetter(string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+export function listWithAnd(arr: string[]) {
+  if (arr.length === 0) return "";
+  if (arr.length === 1) return arr[0];
+  if (arr.length === 2) return `${arr[0]} and ${arr[1]}`;
+  return `${arr.slice(0, -1).join(", ")}, and ${arr.at(-1)}`;
+}
