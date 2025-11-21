7283c58d59b94f2a91865dfc079e9d42435937db
A-J Roos <asjasroos@pm.me>
2025-11-21 10:06:04 +0200

    fix(query-client): set staleTime to Infinity for queries
    Signed-off-by: A-J Roos <asjasroos@pm.me>

--- apps/web/src/lib/query.client.ts
+++ apps/web/src/lib/query.client.ts
@@ -1,12 +1,10 @@
 import { QueryClient } from "@tanstack/react-query";
 
-const STALE_TIME = 5 * 60 * 1000; // 5 minutes
-
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: 1,
-      staleTime: STALE_TIME,
+      staleTime: Infinity,
     },
   },
 });
