66315381cb70ba5b1934c776f908cd3722bedc93
A-J Roos <asjasroos@pm.me>
2026-03-03 04:58:23 +0200

    fix: update login success verification to check URL redirection
    Signed-off-by: A-J Roos <asjasroos@pm.me>

--- apps/web/cypress.config.ts
+++ apps/web/cypress.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:4173",
+    viewportWidth: 1280,
+    viewportHeight: 720,
     retries: {
       runMode: 2,
       openMode: 0,
